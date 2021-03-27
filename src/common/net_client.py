03e0033c3120843016273df5a3e097c1932175de
squirrelRao <hqraop@163.com>
2021-03-27 22:46:53 +0800

    commit code

--- src/common/net_client.py
+++ src/common/net_client.py
@@ -4,18 +4,31 @@ import sys
 import urllib.request as req
 import json
 import datetime
-
+import requests
     
 class NetClient:
 
-    #request network url
-    def request(self,url,data={},method="POST",header={'Content-Type':'application/json'},coding='utf-8'):
-        data = json.dumps(data).encode(coding) 
-        request = req.Request(url,data = data,headers = header,method = method)
-        response = req.urlopen(request)
-        rs = response.read().decode(coding)
-        return rs
+    def __init__(self):
+        self.session = requests.Session()
 
+    def get(self,url,res_type="json"):
+        res = self.session.get(url)
+        if res_type =="json":
+            return res.json()
+        elif res_type == "text":
+            return res.text
+        else:
+            return res.content
+
+    def post(self,url,data,header={'Content-Type':'application/json'},res_type="json"):
+        data = json.dumps(data) 
+        res = self.session.post(url,data = data,header = header)
+        if res_type == "json":
+            return res.json()
+        elif res_type == "text":
+            return res.text
+        else:
+            return res.content
 
 
 net_client = NetClient()
--- src/model/huobi.py
+++ src/model/huobi.py
@@ -7,20 +7,33 @@ from common.net_client import net_client
 class HuobiTrade:
 
     def __init__(self):
-        self.host = "http://api-aws.huobi.pro" 
-        self.host = "http://api.huobi.pro"
+        self.host = "https://api-aws.huobi.pro" 
+        self.host = "https://api.huobi.pro"
         self.access_key = "00f2bf63-703e6631-dbye2sf5t7-7f94b"
         self.secret_key = "795f1d60-25eee502-07545a80-1c76f"
 
-    def get_kline(self,symbol,period = "1min",size=200): #period in 1min, 5min, 15min, 30min, 60min, 4hour, 1day, 1mon, 1week, 1year
+    def get_kline(self,symbol,period = "1min",size=10): #period in 1min, 5min, 15min, 30min, 60min, 4hour, 1day, 1mon, 1week, 1year
         url = self.host + "/market/history/kline?symbol="+symbol+"&period="+period+"&size="+str(size)
-        res = net_client.request(url,method="GET")
-        print(res)
+        print(url)
+        res = net_client.get(url)
+        if res["status"] == "ok":
+            data = {"type":"kline","name":res["ch"],"data":res["data"]}
+            return data
+        return None
 
+    def save_data(self,data):
+        db = mongo_client.fishfin
+        if data["type"] == "kline":
+            for item in data["data"]:
+                item["ktime"] = item["id"]
+                item.pop("id")
+                db.kline.update({"name":data["name"],"ktime":item["id"]},{"$set":item},upsert=True)
+        return 
+    
 
-
+            
 def main():
     x = HuobiTrade()
     x.get_kline("btcusdt")
 
-main()
+#main()
