7720119e97fc971474c0fac8da98edf04e6ef927
squirrelRao <hqraop@163.com>
2021-03-29 16:26:07 +0800

    -

--- src/script/sync_kline_data_per5min.py
+++ src/script/sync_kline_data_per5min.py
@@ -14,27 +14,12 @@ host = "http://lab.lakewater.cn"
 url = host +"/v1/trade/data/push"
 period = "1min"
 
-now_min = datetime.datetime.now().minute
-
 _list = net_client.get(host +"/v1/symbol/watch/list")
 if _list["rc"] == 0:
     #sync 1min data
-    period = "1min"
     for symbol in _list["data"]:
         print("sync kline data:" + symbol + " "+period)
         data = huobi.get_kline(symbol = symbol,period = period)
         print(data)
         net_client.post(url,data)
 
-
-    #sync 5min data
-    if now_min%5 == 0:
-        period = "5min"
-        for symbol in _list["data"]:
-            print("sync kline data:" + symbol + " "+period) 
-            data = huobi.get_kline(symbol = symbol,period = period)
-            print(data)
-            net_client.post(url,data)
-
-
-    #sync 15
